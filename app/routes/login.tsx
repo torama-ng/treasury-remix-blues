e6918b69535c8008b87b5afa742254c1b2bbbab5
Akpodigha Filatei <filatei@gtsng.com>
2023-05-27 13:23:03 +0100

    comment out login route

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,70 +1,70 @@
-import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
-import { json, redirect } from "@remix-run/node";
+// import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
+// import { json, redirect } from "@remix-run/node";
 
-import { verifyLogin } from "~/models/user.server";
-import LoginPage from "~/pages/LoginPage";
-import { createUserSession, getUserId } from "~/services/session.server";
-import { safeRedirect, validateEmail } from "~/utils/utils";
-import { auth, signInWithGooglePopup } from "../utils/firebase.utils";
+// import { verifyLogin } from "~/models/user.server";
+// import LoginPage from "~/pages/LoginPage";
+// import { createUserSession, getUserId } from "~/services/session.server";
+// import { safeRedirect, validateEmail } from "~/utils/utils";
+// import { auth, signInWithGooglePopup } from "../utils/firebase.utils";
 
-export const loader = async ({ request }: LoaderArgs) => {
-  const userId = await getUserId(request);
-  if (userId) return redirect("/");
-  return json({});
-};
+// export const loader = async ({ request }: LoaderArgs) => {
+//   const userId = await getUserId(request);
+//   if (userId) return redirect("/");
+//   return json({});
+// };
 
-export const action = async ({ request }: ActionArgs) => {
-  const formData = await request.formData();
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
-  const remember = formData.get("remember");
+// export const action = async ({ request }: ActionArgs) => {
+//   const formData = await request.formData();
+//   const email = formData.get("email");
+//   const password = formData.get("password");
+//   const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
+//   const remember = formData.get("remember");
 
-  if (!validateEmail(email)) {
-    return json(
-      { errors: { email: "Email is invalid", password: null } },
-      { status: 400 }
-    );
-  }
+//   if (!validateEmail(email)) {
+//     return json(
+//       { errors: { email: "Email is invalid", password: null } },
+//       { status: 400 }
+//     );
+//   }
 
-  if (typeof password !== "string" || password.length === 0) {
-    return json(
-      { errors: { email: null, password: "Password is required" } },
-      { status: 400 }
-    );
-  }
+//   if (typeof password !== "string" || password.length === 0) {
+//     return json(
+//       { errors: { email: null, password: "Password is required" } },
+//       { status: 400 }
+//     );
+//   }
 
-  if (password.length < 8) {
-    return json(
-      { errors: { email: null, password: "Password is too short" } },
-      { status: 400 }
-    );
-  }
+//   if (password.length < 8) {
+//     return json(
+//       { errors: { email: null, password: "Password is too short" } },
+//       { status: 400 }
+//     );
+//   }
 
-  const user = await verifyLogin(email, password);
+//   const user = await verifyLogin(email, password);
 
-  if (!user) {
-    return json(
-      { errors: { email: "Invalid email or password", password: null } },
-      { status: 400 }
-    );
-  }
+//   if (!user) {
+//     return json(
+//       { errors: { email: "Invalid email or password", password: null } },
+//       { status: 400 }
+//     );
+//   }
 
-  return createUserSession({
-    redirectTo,
-    remember: remember === "on" ? true : false,
-    request,
-    userId: user.id,
+//   return createUserSession({
+//     redirectTo,
+//     remember: remember === "on" ? true : false,
+//     request,
+//     userId: user.id,
     
-  });
-};
+//   });
+// };
 
-export const meta: V2_MetaFunction = () => [{ title: "Login" }];
+// export const meta: V2_MetaFunction = () => [{ title: "Login" }];
 
-export default function Login() {
-  return (
-    <div className="container mx-auto">  <LoginPage /> </div>
+// export default function Login() {
+//   return (
+//     <div className="container mx-auto">  <LoginPage /> </div>
 
-  );
-}
+//   );
+// }
 
